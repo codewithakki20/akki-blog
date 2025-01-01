da4f415226326654fe3bfdaf549052ba4b67de37
Ankit Patel <ankitpatel657843@gmail.com>
2025-01-01 16:11:46 +0530

    module.expor

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import pluginJs from "@eslint/js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname, // Resolve paths for `extends`
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"), // Convert traditional config
-  {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"], // Apply to JS/TS files
-    rules: {
-      "no-unused-vars": "warn",
-      "react/react-in-jsx-scope": "off", // Example: Custom rule
-    },
-  },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
 ];
-
-export default eslintConfig;
